c32ed252df8fe19a4cdf7465d547407e4f35fb73
Thomas Austin <thomasaustin1@hotmail.co.ukl>
2022-03-22 19:40:27 +0000

    Began implementing application layer for Jobs
    *Added query and commands for mediator
    *Added typed repository for retrieving job information

---
+++ BlueSquare.Jobs.Application/Queries/GetJobByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using BlueSquare.Domain.Dtos;
+using MediatR;
+
+namespace BlueSquare.Jobs.Application.Queries
+{
+    public class GetJobByIdQuery : IRequest<JobDto>
+    {
+        public int JobId { get; set; }
+    }
+}
---
+++ BlueSquare.Jobs.Application/Repositories/JobRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlueSquare.Domain.Entities;
+using BlueSquare.Infrastructure.Repositories;
+
+namespace BlueSquare.Jobs.Application.Repositories
+{
+    public class JobRepository : BaseRepository<Job>, IJobRepository
+    {
+    }
+}
