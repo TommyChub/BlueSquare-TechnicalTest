e9b9380bada5db2be936f7475910e76b1fcc8bb6
Thomas Austin <thomasaustin1@hotmail.co.ukl>
2022-03-22 18:04:09 +0000

    InitialCommit & Basic Setup

---
+++ BlueSquare.Jobs.Api/Controllers/JobController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueSquare.Jobs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public JobController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult GetJobs()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetJobsById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult CreateJob()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateJob()
+        {
+            return Ok();
+        }
+    }
+}
